0b61cda6934e8e1329d914af154ac56bfbe82311
Harry <harry.caldwell.rose@gmail.com>
2022-10-28 16:32:43 -0400

    Seq Macro WIP (on step 3)

--- seq/src/lib.rs
+++ seq/src/lib.rs
@@ -1,8 +1,45 @@
 use proc_macro::TokenStream;
 
+use syn::Token;
+use syn::parse::{Parse, ParseStream};
+use syn::parse_macro_input;
+
+struct Seq {
+    pub ident: syn::Ident,
+    pub start: i64,
+    pub end: i64,
+    pub block: proc_macro2::TokenStream,
+}
+
+impl Parse for Seq {
+    fn parse(input: ParseStream) -> syn::Result<Self> {
+        let ident = input.parse()?;
+        input.parse::<Token![in]>()?;
+        let start_lit: syn::LitInt = input.parse()?;
+        let start = start_lit.base10_parse()?;
+        input.parse::<Token![..]>()?;
+        let end_lit: syn::LitInt = input.parse()?;
+        let end = end_lit.base10_parse()?;
+        let content;
+        syn::braced!(content in input);
+        let block = content.parse()?;
+        Ok(Seq {
+            ident,
+            start,
+            end,
+            block,
+        })
+    }
+}
+
 #[proc_macro]
 pub fn seq(input: TokenStream) -> TokenStream {
-    let _ = input;
+    let input = parse_macro_input!(input as Seq);
+    eprintln!("TOKENS: {}", input.block);
 
-    unimplemented!()
+    let seq_tokens = (input.start..input.end).map(|n| {
+        
+    });
+
+    TokenStream::new()
 }
